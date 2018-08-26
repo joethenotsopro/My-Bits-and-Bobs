ce7f30902d0cabbf4bd38bff2cc5b0bdd9908057
joethenotsopro <42708890+joethenotsopro@users.noreply.github.com>
2018-08-26 11:20:51 +0100

    Update compress_pdf.sh
    This small script compresses PDF files into smaller files. It's my modification to a line of code I found on the Intertubes.

--- compress_pdf.sh
+++ compress_pdf.sh
@@ -1,4 +1,21 @@
+# To use it, copy the 4 lines of code (lines 9-12) into a file called compress_pdf.sh and then:
+#
+# cp compress_pdf.sh $HOME/bin
+# chmod +x $HOME/bin/compress_pdf.sh
+#
+# Open up a terminal window in your PDF files directory and then run the script... the script creates a directory
+# called $HOME/pdf_output # then compresses the PDF's into that directory. It leaves the originals intact.
+
+
 #!/bin/bash
 var=$(pwd)
 mkdir $HOME/pdf_output
 for f in *.pdf; do gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -sOutputFile="$HOME/pdf_output/$(basename "$f")" "$var"/"$f" ; done
+
+
+# You change -dPDFSETTINGS= to adjust the compression settings:
+#
+# /screen – Lowest quality, lowest size
+# /ebook – Moderate quality
+# /printer – Good quality
+# /prepress – Best quality, highest size
